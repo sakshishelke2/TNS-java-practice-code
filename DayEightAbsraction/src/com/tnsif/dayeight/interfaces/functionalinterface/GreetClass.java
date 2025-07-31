47080670bffd35369c2658edcf7a0d2c7e850c44
sakshi <sakshi@LAPTOP-9JFL93DO>
2025-07-31 23:19:50 +0530

    Abstraction

---
+++ DayEightAbsraction/src/com/tnsif/dayeight/interfaces/functionalinterface/GreetClass.java
@@ -0,0 +1,9 @@
+package com.tnsif.dayeight.interfaces.functionalinterface;
+
+public class GreetClass implements Greetings{
+
+	@Override
+	public String greet() {
+		return "Welcome to the World of Java";
+	}
+}
---
+++ DayEightAbsraction/src/com/tnsif/dayeight/interfaces/functionalinterface/Greetings.java
@@ -0,0 +1,14 @@
+package com.tnsif.dayeight.interfaces.functionalinterface;
+
+public interface Greetings {
+
+public String greet();
+	
+	//public void show();
+	
+	static void show()
+	{
+		
+	}
+
+}

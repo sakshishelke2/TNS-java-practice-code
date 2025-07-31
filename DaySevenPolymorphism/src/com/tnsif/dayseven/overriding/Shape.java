facc40bac65bfdb8e0b3d6f7f329e6ffb042865b
sakshi <sakshi@LAPTOP-9JFL93DO>
2025-07-31 23:17:56 +0530

    Polymorphism

---
+++ DaySevenPolymorphism/src/com/tnsif/dayseven/overriding/Shape.java
@@ -0,0 +1,15 @@
+package com.tnsif.dayseven.overriding;
+
+public class Shape {
+
+	// member funtion
+		public void draw()
+		{
+			System.out.println("Drawing a generic shape");
+		}
+
+		public void erase()
+		{
+			System.out.println("Erasing a generic shape");	
+		}
+}
